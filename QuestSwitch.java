50cdbca39a241cedf79802e9051db252746de03b
RadityaArva <arvamessi@gmail.com>
2025-11-05 20:17:17 +0700

    menambahkan deklarasi tipe data

--- QuestSwitch.java
+++ QuestSwitch.java
@@ -5,6 +5,9 @@ public class QuestSwitch {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        double nilaiSd, nilaiPbo, nilaiBd, nilaiAlgo, nilaiRpl, rataRata, totalNilai;
+        String grade, mataKuliah;
+
         console.close();
     }
     
