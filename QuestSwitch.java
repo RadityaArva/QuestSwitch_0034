fc3dcf8973396255702cdc7c42f58b4f82ea3dac
RadityaArva <arvamessi@gmail.com>
2025-11-05 21:05:06 +0700

    penambahan pengkondisian dan penentuan matkul

--- QuestSwitch.java
+++ QuestSwitch.java
@@ -22,12 +22,12 @@ public class QuestSwitch {
         nilaiRpl = console.nextDouble();
 
         totalNilai = nilaiSd + nilaiPbo + nilaiBd + nilaiAlgo + nilaiRpl;
-        rataRata = totalNilai / 5;
+        rataRata = (totalNilai / 5) / 25;
 
         System.out.println("Total Nilai dan Rata-Rata Anda :");
         System.out.println("================================");
         System.out.println("Total Nilai :" +totalNilai);
-        System.out.println("Total Rata-Rata :" +rataRata);
+        System.out.println("Total (IPK skala 4) Anda Adalah :" +rataRata);
 
         if (rataRata >= 3.75) {
             grade = "A (Sangat Baik)";
@@ -39,6 +39,46 @@ public class QuestSwitch {
             grade = "BC (Cukup)";
         } else {
             grade = "C (Kurang)";
+        }
+
+        System.out.println("Predikat Akademi Anda Adalah =" +grade);
+
+        if(grade.startsWith("A") || grade.startsWith("AB")){
+            System.out.println("Selamat anda dinyatakan lolos untuk menjadi Asisten Dosen");
+            System.out.println("========================================================");
+
+            console.nextLine();
+
+            System.out.println("Silahkan pilih mata kuliah untuk menjadi Asisten Dosen");
+            System.out.println("1. Struktur Data");
+            System.out.println("2. Pbo");
+            System.out.println("3. Basis Data");
+            System.out.println("4. Algoritma");
+            System.out.println("5. Rekayasa Perangkat Lunak");
+
+            int pilihan = console.nextInt();
+
+            switch (pilihan) {
+                case 1:
+                    mataKuliah = "Struktur Data";
+                    break;
+                case 2:
+                    mataKuliah = "Pemrogramman Berbasi Objek";
+                    break;
+                case 3:
+                    mataKuliah = "Basis Data";
+                    break;
+                case 4:
+                    mataKuliah = "Algoritma";
+                    break;
+                case 5:
+                    mataKuliah = "Rekayasa Perangkat Lunak";
+                    break;
+            
+                default:
+                    System.out.println("Pilihan Invalid");
+                    break;
+            }
         }
 
         console.close();
