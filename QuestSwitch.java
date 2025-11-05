a405aa7a1f405e03e5f9294a1af33e9d6481c049
RadityaArva <arvamessi@gmail.com>
2025-11-05 20:16:00 +0700

    Menambahkan inisialisasi Scanner untuk input user

--- QuestSwitch.java
+++ QuestSwitch.java
@@ -2,5 +2,10 @@ import java.util.Scanner;
 
 public class QuestSwitch {
 
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        console.close();
+    }
     
 }
